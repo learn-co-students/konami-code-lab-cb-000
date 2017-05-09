e851e0b44983ce3441d844f0e7b4e9fe9bc9dce2
Marina Paskal <paskalm@rogers.com>
2017-05-09 18:31:37 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,4 +2,14 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // your code here
+  document.addEventListener("keydown", function onKeyDownHandler(e) {
+    var index = 0;
+    const key = parseInt(e.detail || e.which)
+    for(var i = 0; i < code.length; i++){
+      if (key === code[index]) {
+        alert("Congratulations! You are right!");      
+      }
+    }     
+});
 }
+init();
