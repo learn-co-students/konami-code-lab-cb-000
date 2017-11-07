0ed197c760249ea2f4caf48c69980b54b37ce3f5
Chris Nunez <cenunez47@gmail.com>
2017-11-07 21:51:28 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,24 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
-  var i = 0;
+  var breaker = 0;
 
-  document.body.addEventListener('keydown', function(e) {
-    var stroke = parseInt(e.detail);
+  document.body.addEventListener('keydown', function(event) {
+    var stroke = parseInt(event.detail); //stroke is defined as an integer of the keydown event
 
-    if (code[i] === stroke) {
-      i++;
+    if (code[breaker] === stroke) { //if the key pressed is equal to the array index increment breaker by 1
+      breaker++;
 
-      if (i === code.length) {
+      if (breaker === code.length) { //if breaker integer is equal the length of the code array, alert the user they completed the code and reset i to 0
         alert('Hurray!');
-        i = 0;
+        breaker = 0;
       }
     }
 
-    else {
-      i = 0;
+    else { //if key pressed is not equal the code array index, reset breaker to 0
+      breaker = 0;
     }
-    
+
   });
 
 }
