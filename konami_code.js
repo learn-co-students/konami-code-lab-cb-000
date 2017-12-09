ecadfa2973d751dd0d153bbe380b449a94df6777
cptbrkfst <dyl@resonategroup.ca>
2017-12-09 12:24:43 -0500

    done lol

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,15 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  let i = 0
 
+  document.body.addEventListener('keydown', event => {
+    const keyName = parseInt(event.which || event.detail)
+
+    code[i] === keyName ? i++ : (i = 0)
+
+    if (i === code.length) {
+      alert('Hurray!')
+    }
+  })
 }
