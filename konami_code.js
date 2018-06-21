f65db19f567dd075bf41b0f9462af1d6009db60e
Emma Konrad <emma.konrad92@gmail.com>
2018-06-21 14:35:12 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,23 @@
-const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
+const codes = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
+let index = 0;
 
 function init() {
-  // your code here
+  const body = document.getElementByTagName('body')
+  
+  body.addEventListener('keydown', function onKeyDownHandler(e){
+    var stroke = parseInt(event.detail);
+    if (stroke === codes[index]) {
+      index++;
+      
+    if (index === codes.length) {
+      alert("Hurray!");
+      
+      index = 0;
+    }
+    } else {
+      index = 0;
+    }
+    }
+  )
 }
