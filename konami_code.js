7045a56b181f625ee7ffc3adb30c8cd0fd1ab8cb
Hendrik Tammekivi <tammekivi.hendrik@gmail.com>
2018-09-23 19:58:11 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,3 +1,5 @@
+Konami
+
 const codes = [
   "ArrowUp",
   "ArrowUp",
@@ -12,5 +14,23 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  var index = 0;
+  const input = document.querySelector('body')
+
+  input.addEventListener('keydown', function(e) {
+
+      const key = e.key;
+
+      if (key === codes[index]) {
+        index++;
+
+        if (index === codes.length) {
+          alert('Congratz!');
+
+          index = 0;
+        }
+      } else {
+        index = 0;
+    }
+  });
 }
