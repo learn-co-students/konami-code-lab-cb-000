3fbba8d2056a197d948031951cc971e1cdcedb39
Joe Corwin <joe.corwin@gmail.com>
2019-01-31 03:37:26 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,24 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
-}
+
+ function init() {
+   var index = 0;
+   document.body.addEventListener('keydown', function onKeyDownHandler(e){
+
+        const key = e.key;
+
+        if (key === codes[index]) {
+         index++;
+
+        if (index === codes.length) {
+         window.alert("Hurray!");
+
+          index = 0;
+       }
+       } else {
+         index = 0;
+       }
+       }
+     )
+   }
