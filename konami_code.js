592970e466b020c299ee252b7923c460ace8eb41
Joe Corwin <joe.corwin@gmail.com>
2019-01-31 03:35:41 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,35 +1,34 @@
-
 const codes = [
-  "ArrowUp",	  "ArrowUp",
-  "ArrowUp",	  "ArrowUp",
-  "ArrowDown",	  "ArrowDown",
-  "ArrowDown",	  "ArrowDown",
-  "ArrowLeft",	  "ArrowLeft",
-  "ArrowRight",	  "ArrowRight",
-  "ArrowLeft",	  "ArrowLeft",
-  "ArrowRight",	  "ArrowRight",
-  "b",	  "b",
-  "a"	  "a"
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a"
 ];
 
 
  function init() {
    var index = 0;
-  document.body.addEventListener('keydown', function onKeyDownHandler(e){
+   document.body.addEventListener('keydown', function onKeyDownHandler(e){
 
-     const key = e.key;
+        const key = e.key;
 
-     if (key === codes[index]) {
-      index++;
+        if (key === codes[index]) {
+         index++;
 
-     if (index === codes.length) {
-      window.alert("Hurray!");
+        if (index === codes.length) {
+         window.alert("Hurray!");
 
-       index = 0;
-    }
-    } else {
-      index = 0;
-    }
-    }
-  )
-}
+          index = 0;
+       }
+       } else {
+         index = 0;
+       }
+       }
+     )
+   }
