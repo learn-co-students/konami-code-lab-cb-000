a7a31f0e25f11653f867c00ad2d129d1f0f245db
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-20 18:46:02 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,15 @@ const codes = [
   "a"
 ];
 
+let index = 0
+
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', kcode);
+}
+
+function kcode(e) {
+  let key = e.key;
+  if (key === codes[index] && key === "a") {alert("Nice job inputting the code")}
+  else if (key === codes[index]) {index += 1}
+  else {index = 0}
 }
